b9c2d43987cd5eb7a3e4f243e06bb6311007d600
ericHumberto <eric.c.humberto@gmail.com>
2017-02-21 22:18:53 -0300

    Hidding api key

---
+++ app/src/main/java/com/udacity/erics/popularmovies/util/ApiKey.java
@@ -0,0 +1,11 @@
+package com.udacity.erics.popularmovies.util;
+
+public class ApiKey {
+
+    private static final String KEY = "xxxxxx";
+
+    public static String getKey() {
+        return KEY;
+    }
+
+}
